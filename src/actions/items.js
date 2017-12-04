61e6dd4dfa5d9c50581a2eb5e8ef6b36810357e3
ian morton <ian.happytuesdays@gmail.com>
2017-12-04 19:23:59 +0000

    final fetch actions

--- src/actions/items.js
+++ src/actions/items.js
@@ -16,3 +16,20 @@ export function itemsFetchDataSuccess(items) {
         items
     };
 }
+
+export function itemsFetchData(url) {
+    return (dispatch) => {
+        dispatch(itemsIsLoading(true));
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                dispatch(itemsIsLoading(false));
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => dispatch(itemsFetchDataSuccess(items)))
+            .catch(() => dispatch(itemsHasErrored(true)));
+    };
+}
